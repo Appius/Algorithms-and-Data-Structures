0ba0cf42405aebaec0ae307b60d29150fd6ef6e2
Appius1843 <appius@outlook.com>
2013-11-17 23:51:16 +0200

    Add new project for sorting algorithms. Add first three algorithms - bubble, insertion and selection ones.

---
+++ Algorithms/SortingAlgorimths/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorimths
+{
+    class Program
+    {
+        private static void Display(params int[] data)
+        {
+            Console.WriteLine(string.Join(" ",data));
+        }
+
+        static void Main(string[] args)
+        {
+            var data = new[] {1, 6, 9, 4, 7, -6, 7};
+            Display(data);
+            
+//            Display(SortingAlgorithms<int>.BubbleSort(data));
+//            Display(SortingAlgorithms<int>.InsertionSort(data));
+            Display(SortingAlgorithms<int>.SelectionSort(data));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithms/SortingAlgorimths/SortingAlgorithms.cs
@@ -0,0 +1,91 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace SortingAlgorimths
+{
+    /// <summary>
+    ///     Алгоритмы сортировки
+    /// </summary>
+    /// <typeparam name="T">Тип данных, коллекцию которых надо отсортировать</typeparam>
+    public static class SortingAlgorithms<T> where T : IComparable
+    {
+        /// <summary>
+        ///     Сортировка пузырем
+        /// </summary>
+        /// <param name="array">Массив данных</param>
+        public static T[] BubbleSort(params T[] array)
+        {
+            for (int n = array.Length - 1; n >= 0; n--)
+            {
+                var isSwaped = false;
+                for (int i = 0; i < n; i++)
+                {
+                    if (array[i].CompareTo(array[i + 1]) > 0)
+                    {
+                        T temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                        isSwaped = true;
+                    }
+                }
+                if (!isSwaped) break;
+            }
+            return array;
+        }
+
+        /// <summary>
+        ///     Сортировка вставками
+        /// </summary>
+        /// <param name="array">Коллекция данных</param>
+        public static T[] InsertionSort(params T[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i].CompareTo(array[i - 1]) < 0)
+                {
+                    var temp = array[i];
+                    var j = i - 1;
+                    while (j >= 0 && array[j].CompareTo(temp) > 0)
+                    {
+                        array[j + 1] = array[j];
+                        j--;
+                    }
+                    array[j + 1] = temp;
+                }
+            }
+            return array;
+        }
+
+        /// <summary>
+        ///     Сортировка выборкой
+        /// </summary>
+        /// <param name="array">Коллекция данных</param>
+        public static T[] SelectionSort(params T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                T min = array[i];
+                int index = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(min) < 0)
+                    {
+                        min = array[j];
+                        index = j;
+                    }
+                }
+                int k = index - 1;
+                while (k >= i)
+                {
+                    array[k + 1] = array[k];
+                    k--;
+                }
+                array[k + 1] = min;
+            }
+            return array;
+        }
+    }
+}
